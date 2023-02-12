d46520bea7e4e3c0a6693fda5af62812e8e7cf6b
Alex Mandebura <xenovidbis@gmail.com>
2023-02-12 22:11:54 +0200

    third edition
    looking for tutorial how to merge your pull request

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,10 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import Searchbar from './Searchbar/Searchbar';
 import Modal from './Modal/Modal';
-import  * as API from '../services/api';
+import * as API from '../services/api';
 import './App.css';
 
-
-
 export default class App extends Component {
- 
-
   state = {
     page: 1,
     searchName: '',
@@ -19,85 +15,90 @@ export default class App extends Component {
     items: [],
     isLoading: false,
     error: null,
-  }
+    sb: false,
+  };
 
-  openModalOpen = ({target}) => {
+  openModalOpen = ({ target }) => {
     this.setState({
       largeImage: target.dataset.src,
-    })
-  }
+    });
+  };
 
   onModalClose = () => {
     this.setState({
       largeImage: '',
-    })
-  }
+    });
+  };
 
-  handleFormSubmit = (searchName) => {
-    if(searchName.trim().length === 0) {
+  handleFormSubmit = searchName => {
+    if (searchName.trim().length === 0) {
       alert('Please, enter request');
-      return
+      return;
     }
 
     this.setState({
       searchName,
       page: 1,
       items: [],
-    })
-  }
+    });
+  };
 
   handleLoadMore = () => {
     this.setState(prevState => ({
       page: prevState.page + 1,
-    }))
-  }
+    }));
+  };
 
   getImages = async (searchName, page) => {
     try {
       this.setState({
         isLoading: true,
       });
-      const images = await API.loadImage(searchName, page);
+      const { images, totalHits } = await API.loadImage(searchName, page);
 
       this.setState(prevState => ({
         items: [...prevState.items, ...images],
-        isLoading: false,
+        sb: this.state.page < Math.ceil(totalHits / 12),
       }));
       if (images.length === 0) {
-        alert("Sorry, we can't find anyting for your request. Please, enter another request");
-        }
-      } catch (error) {
-        this.setState({
-          error: error.message,
-        })
-      }  finally {
-        this.setState({
-          isLoading: false,
-        });
+        alert(
+          "Sorry, we can't find anyting for your request. Please, enter another request"
+        );
       }
-    };
-  
-    componentDidUpdate (_, prevState) {
-      if (prevState.page !== this.state.page || 
-         prevState.searchName !== this.state.searchName) {
-        this.getImages(this.state.searchName, this.state.page);
-      }
+    } catch (error) {
+      this.setState({
+        error: error.message,
+      });
+    } finally {
+      this.setState({
+        isLoading: false,
+      });
     }
+  };
 
-
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.page !== this.state.page ||
+      prevState.searchName !== this.state.searchName
+    ) {
+      this.getImages(this.state.searchName, this.state.page);
+    }
+  }
 
   render() {
-    const { items, largeImage, isLoading, error} = this.state;
+    const { items, largeImage, isLoading, error, sb } = this.state;
 
     return (
       <div className="App">
-              <Searchbar onSubmit={this.handleFormSubmit} isLoading={isLoading}/>
-              {error && <p>{error}</p>}
-               {items.length > 0 &&  <ImageGallery items={items}  onClick={this.openModalOpen} />}
-               {isLoading && <Loader />}
-               {items.length >= 12 && <Button onClick={this.handleLoadMore} isLoading={isLoading}/>}
-              {largeImage && (<Modal onClose={this.onModalClose} url={largeImage} />)}
-             </div>
+        <Searchbar onSubmit={this.handleFormSubmit} isLoading={isLoading} />
+        {error && <p>{error}</p>}
+        {items.length > 0 && (
+          <ImageGallery items={items} onClick={this.openModalOpen} />
+        )}
+        {isLoading && <Loader />}
+        {sb && <Button onClick={this.handleLoadMore} isLoading={isLoading} />}
+        {largeImage && <Modal onClose={this.onModalClose} url={largeImage} />}
+      </div>
     );
   }
 }
